abaadf40bb1725d62401115c07d535681c48e3ff
AgustinTafura <agustintafura@hotmail.com>
2021-05-10 14:24:17 -0300

    correccion fuente flamenco-regular

--- src/components/homeSection/index.jsx
+++ src/components/homeSection/index.jsx
@@ -10,7 +10,7 @@ const HomeSection = () => {
                 <div className="row">
                     <div className="col-md-12 col-sm-12">
                     <a href="#" className="navbar-brand"> <img src={logo} alt="logo-belgica-saludable" style={{width: "7rem"}} /></a>
-                        <h2 className="wow bounce animate__animated animate__bounce" style={{fontFamily:'Flamenco Regular',fontSize:'7rem'}}>WAFFLES</h2>
+                        <h2 className="wow bounce animate__animated animate__bounce" style={{fontFamily:'Flamenco-Regular',fontSize:'7rem'}}>WAFFLES</h2>
                         <a href="#intro" className="btn btn-default smoothScroll">QUIERO PROBARLOS</a>
                     </div>
                 </div>
